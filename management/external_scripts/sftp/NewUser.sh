6c27dd11975477b25d88c5dac33bcf1fa812860e
Scooby <abereg01@pm.me>
2024-09-25 13:37:03 +0200

    new install

--- management/external_scripts/sftp/NewUser.sh
+++ management/external_scripts/sftp/NewUser.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Read configuration from JSON file
+config_file="NewUser.json"
+ssh_user=$(jq -r '.ssh_user' "$config_file")
+ssh_host=$(jq -r '.ssh_host' "$config_file")
+script_path=$(jq -r '.script_path' "$config_file")
+
 # SSH into the specified host
-ssh digit@172.20.96.22 << EOF
+ssh "${ssh_user}@${ssh_host}" << EOF
     # Run the Python script with sudo
-    sudo python3 \$HOME/digit/scripts/useradd.py
+    sudo python3 \$HOME${script_path}
     
     # Exit the SSH session
     exit
 EOF
-
 # Exit the local script
 exit 0
